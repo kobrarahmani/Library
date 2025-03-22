c3e72489566d09a4ec7dcec366e26c31438661ef
kobrarahmani <122036825+kobrarahmani@users.noreply.github.com>
2025-03-22 12:46:45 +0330

    Create LoanBook.h

---
+++ LoanBook.h
@@ -0,0 +1,41 @@
+//#pragma once
+//#include"Books.h"
+//#include"Members.h"
+//#include"Date.h"
+//class LoanBooks:Books,Members
+//{
+//private:
+//	int codeL;
+//	int codeB;
+//	int codeM;
+//	Date taDate;//taken
+//	Date reDate;//return
+//public:
+//	LoanBooks();
+//	LoanBooks(int,int,int,Date t(int,int,int),Date r(int,int,int));
+//	void setCL(int);
+//	void setML(int);
+//	void setL(int);
+//	void setTa(Date ta(int,int,int));
+//	void setRe(Date re(int,int,int));
+//	Date getTa() { return taDate; };
+//	Date getReDate() {return reDate;};
+//	bool checkB(int);
+//	bool checkM(int);
+//	int  getCL() {return codeB;}
+//	int getML() { return codeM; };
+//	int getL() { return codeL; };
+//	void inLBook(vector <LoanBooks> &);//inserting book specifications
+//	void fLBook(vector<LoanBooks> &);
+//	void reLBook();//print books list in a file
+//	void seLBook(vector<LoanBooks> &, int);
+//	void edLBook(vector<LoanBooks> &);//for editing books info
+//	void edFLBook(vector<LoanBooks> &);
+//	void delLId(int);//for editing id
+//	void vecLBook(vector<LoanBooks> &);
+//	bool seSameLC(vector<LoanBooks> &, int);
+//	void seIdLBook(vector<LoanBooks> &); //reading from file
+//	void delNaIdLBook(vector<LoanBooks> &, int);
+//	~LoanBooks();
+//};
+//
